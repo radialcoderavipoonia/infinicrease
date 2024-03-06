41f09ab0103ca42fb4d42ceb13e51855e1be8133
sachin-niyana <sachinjakhar100@gmail.com>
2024-03-06 12:31:04 +0530

    take pull from dev

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Footer from "@/components/Footer";
+import Genie from "@/components/Genie";
 
 export default function Home() {
   return (
     <>
       <Footer />
+      <Genie />
     </>
   );
 }
