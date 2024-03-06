8a3fdd6a71dedde7d8e0235a578549f079668a3c
radialcoderavipoonia <ravipoonia28122002@gmail.com>
2024-03-06 15:11:21 +0530

    css code removed

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -19,31 +19,31 @@ const Roadmap = () => {
             <Image
               height={171}
               width={119}
-              className="absolute roadmap_first_number_position md:w-[119px] md:h-[171px] sm:w-[99px] sm:h-[141px] sm:block hidden"
+              className="absolute xl:top-[-33%] xl:left-[6%] md:w-[119px] lg:top-[-32%] lg:left-0 md:top-[-15%] md:left-[4.3%] left-[6.4%] top-[-14%] md:h-[171px] sm:w-[99px] sm:h-[141px] sm:block hidden"
               src="/assets/images/roadmap/roadmap_number_one.png"
             />
             <Image
               height={170}
               width={119}
-              className="absolute roadmap_second_number_position md:w-[119px] md:h-[171px] sm:w-[99px] sm:h-[141px] sm:block hidden"
+              className="absolute xl:top-[-22%] xl:left-[50%] lg:left-[50%] lg:top-[-22%] md:top-[16%] md:left-[4.3%] top-[20%] left-[6.4%] md:w-[119px] md:h-[171px] sm:w-[99px] sm:h-[141px] sm:block hidden"
               src="/assets/images/roadmap/roadmap_number_two.png"
             />
             <Image
               height={194}
               width={119}
-              className="absolute roadmap_third_number_position md:w-[119px] md:h-[194px] sm:w-[99px] sm:h-[161px] sm:block hidden"
+              className="absolute xl:top-[45%] xl:left-[39%] lg:top-[46%] lg:left-[39%] md:top-[42%] md:left-[4.3%] top-[47%] left-[6.4%] md:w-[119px] md:h-[194px] sm:w-[99px] sm:h-[161px] sm:block hidden"
               src="/assets/images/roadmap/roadmap_number_three.png"
             />
             <Image
               height={214}
               width={154}
-              className="absolute roadmap_four_number_position xl:w-[154px] xl:h-[214px] lg:h-[214px] lg:w-[154px] md:w-[119px] md:h-[174px] sm:w-[99px] sm:h-[141px] sm:block hidden"
+              className="absolute xl:top-[57%] xl:left-[83%] lg:top-[57%] lg:left-[88%] md:top-[68%] md:left-[4.3%] top-[70%] left-[6.4%] xl:w-[154px] xl:h-[214px] lg:h-[214px] lg:w-[154px] md:w-[119px] md:h-[174px] sm:w-[99px] sm:h-[141px] sm:block hidden"
               src="/assets/images/roadmap/roadmap_number_four.png"
             />
             <div className="flex justify-between items-start w-full lg:w-[unset]">
               <div className="h-[83%] md:mt-8 mt-4 ms-20 w-[8px] bg-red-500 lg:hidden sm:block hidden"></div>
               <div className="lg:w-full sm:w-3/5 w-full max-w-[310px] mx-auto">
-                <div className="max-w-[306px] lg:absolute phase_one_position">
+                <div className="max-w-[306px] lg:absolute left-[1%] top-[10%]">
                   <h3 className="linear_color font-Nunito font-extrabold text-2xl">
                     Phase 1: Launch
                   </h3>
@@ -68,7 +68,7 @@ const Roadmap = () => {
                     </li>
                   </ul>
                 </div>
-                <div className="max-w-[306px] lg:absolute phase_two_position lg:pt-0 pt-10">
+                <div className="max-w-[306px] lg:absolute xl:left-[60%] left-[62%] top-[8%] lg:pt-0 pt-10">
                   <h3 className="linear_color font-Nunito font-extrabold text-2xl">
                     Phase 2: Growth
                   </h3>
@@ -96,7 +96,7 @@ const Roadmap = () => {
                     </li>
                   </ul>
                 </div>
-                <div className="max-w-[306px] lg:absolute phase_three_position lg:pt-0 pt-10">
+                <div className="max-w-[306px] lg:absolute left-[32%] top-[95%] lg:pt-0 pt-10">
                   <h3 className="linear_color font-Nunito font-extrabold text-2xl">
                     Phase 3: Expansion
                   </h3>
@@ -115,7 +115,7 @@ const Roadmap = () => {
                     </li>
                   </ul>
                 </div>
-                <div className="max-w-[306px] lg:absolute phase_four_position lg:pt-0 pt-10">
+                <div className="max-w-[306px] lg:absolute right-[4%] top-[102%] lg:pt-0 pt-10">
                   <h3 className="linear_color font-Nunito font-extrabold text-2xl">
                     Phase 4: Consolidation
                   </h3>
