a636e5beeb027ebfdd0fe1431819cd98dd3f04c8
kapil verma <vermakp39@gmail.com>
2024-03-06 16:09:10 +0530

    error solving

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,18 +7,18 @@ import { Equalizerbot, GenieIcon, WhaleWallet } from "./common/Icons";
 const Hero = () => {
   return (
     <>
-      <div className=" bg-hero bg-cover bg-no-repeat">
+      <div className=" bg-hero bg-cover bg-no-repeat ">
         <NavBar />
         <div className="container xl:max-w-[1140px] mx-auto px-5 xl:px-0 mt-[109px] pb-[102px]">
-          <div className=" flex flex-wrap relative items-center">
-            <div className="">
+          <div className=" flex flex-col-reverse flex-wrap relative items-center">
+            <div className=" lg:w-7/12 relative z-50">
               <h1 className="text-white  font-black font-Nunito text-7xl leading-[122%]">
                 Infinicrease
               </h1>
-              <p className=" font-Montserrat text-lg text-white opacity-80 mt-4">
+              <p className=" font-Montserrat text-base sm:text-lg text-white opacity-80 mt-4">
                 Infinicrease is a community project on the Binance Smart Chain.
               </p>
-              <p className=" font-Montserrat text-lg text-white opacity-80 mt-[10px] max-w-[597px]">
+              <p className=" font-Montserrat text-base sm:text-lg text-white opacity-80 mt-[10px] w-full max-w-[375px] md:max-w-[597px]">
                 The native INF token is designed to increase in price infinitely
                 regardless of market conditions while incentivising holders with
                 complex tools embedded into the smart contract.{" "}
@@ -26,20 +26,20 @@ const Hero = () => {
               <div className=" mt-7">
                 <CustomButton type="buy" content="Buy INF" />
               </div>
-              <div className=" mt-8 flex">
+              <div className=" mt-8 md:flex">
                 <div className="Equalizerbot rounded-lg w-[188px] h-[149px] flex justify-center items-center flex-col ">
                   <Equalizerbot />
                   <p className=" font-NoFlickingThanks text-base font-normal text-cyanBlack mt-2">
                     Equalizer bot
                   </p>
                 </div>
-                <div className="Equalizerbot rounded-lg w-[188px] h-[149px] flex justify-center items-center flex-col translate-x-[-41px]">
+                <div className="Equalizerbot rounded-lg w-[188px] h-[149px] flex justify-center items-center flex-col sm:translate-x-[-41px]">
                   <WhaleWallet />
                   <p className=" font-NoFlickingThanks text-base font-normal text-cyanBlack mt-2">
                     Whale wallet
                   </p>
                 </div>
-                <div className="Genie rounded-lg w-[188px] h-[149px] flex justify-center items-center flex-col translate-x-[-75px]">
+                <div className="Genie rounded-lg w-[188px] h-[149px] flex justify-center items-center flex-col sm:translate-x-[-75px]">
                   <GenieIcon />
                   <p className=" font-NoFlickingThanks text-base font-normal text-cyanBlack mt-2">
                     Genie
@@ -51,28 +51,27 @@ const Hero = () => {
                   height={106}
                   src="/assets/images/hero/infinicrease_hero_logo.svg"
                   alt="infinicrease_hero_logo"
-                  className=" ms-3 translate-x-[-75px]"
+                  className=" ms-3 sm:translate-x-[-75px]"
                 />
               </div>
             </div>
-            <div className=" w-7/12 absolute right-[-141px] z-10">
-              <div className="">
-                <Image
-                  loading="lazy"
-                  width={347}
-                  height={347}
-                  src="/assets/images/hero/hand_logo.webp"
-                  alt="page_logo"
-                  className="relative ms-20 w-[52%] z-20 hand-ball "
-                />
-              </div>
+            <div className=" lg:w-3/12 xl:w-5/12">
+              <Image
+                loading="lazy"
+                width={347}
+                height={347}
+                src="/assets/images/hero/hand_logo.webp"
+                alt="page_logo"
+                className="relative  md:ms-[60px]  z-20 hand-ball "
+              />
+
               <Image
                 loading="lazy"
                 width={643}
                 height={252}
                 src="/assets/images/hero/hand.png"
                 alt="page_logo"
-                className=" absolute start-0 bottom-[-80px] hand w-full z-[1]"
+                className=" absolute w-[80%] md:w-[74%] bottom-[44%] lg:right-0 lg:bottom-0 lg:w-[51%] xl:right-[-12%] xl:bottom-[-80px] hand xl:w-[60%] z-[1]"
               />
             </div>
           </div>
