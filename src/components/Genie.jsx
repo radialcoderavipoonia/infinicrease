dd79274e5638d30cd6044c9b1ab8a5716bd82a27
sukhvinder-jaat <monudhaka829@gmail.com>
2024-03-07 14:07:07 +0530

    issues in progress

--- src/components/Genie.jsx
+++ src/components/Genie.jsx
@@ -5,7 +5,7 @@ const Genie = () => {
   return (
     <div className="container xl:max-w-[1136px] xl:px-0 px-3 mx-auto sm:pt-[58px] lg:pb-[127px] md:pb-24 sm:pb-16 py-12 relative">
       <div className="w-[250px] h-[600px] bg-gradient-to-r from-[#FC1436] to-[#FFAD00] rotate-[90deg] z-10 rounded-full blur-[150px] absolute left-[-10%] sm:bottom-0 top-0"></div>
-      <div className="grid sm:grid-cols-2 grid-cols-1 items-center gap-4">
+      <div className="grid lg:grid-cols-2 grid-cols-1 items-center gap-4">
         <div className="flex justify-center">
           <Image
             width={346}
@@ -15,10 +15,10 @@ const Genie = () => {
           />
         </div>
         <div className="flex flex-col">
-          <h3 className="text-cyanBlack font-Nunito font-black text-5xl sm:text-start text-center">
+          <h3 className="text-cyanBlack font-Nunito font-black xl:text-5xl sm:text-[45px] text-3xl lg:text-start text-center">
             The <span className="linear_color2">Genie</span>
           </h3>
-          <p className="text-white opacity-80 font-Montserrat font-normal text-base pt-4 sm:max-w-[535px] sm:text-start text-center">
+          <p className="text-white opacity-80 font-Montserrat font-normal text-base pt-4 lg:max-w-[535px] lg:text-start text-center">
             The Genie is a wallet that collects fees and at random times is
             awakened!. When the genie is awakened the first user to go to the
             site claims all the funds in the Genie Wallet. It&#39;s on a first
--- src/components/Stabilies.jsx
+++ src/components/Stabilies.jsx
@@ -13,7 +13,7 @@ const Stabilies = () => {
       />
       <div className="container xl:max-w-[1380px] px-3 mx-auto pt-[42px] pb-[115px] ">
         <div className="flex justify-center items-center flex-row flex-wrap">
-          <div className="md:w-6/12 w-full relative">
+          <div className="lg:w-6/12 w-full relative">
             <Image
               className="absolute bottom-[-23%] left-[-2%]"
               width={647}
@@ -30,12 +30,12 @@ const Stabilies = () => {
               "
             />
           </div>
-          <div className="md:w-6/12 w-full md:mt-0 mt-24">
-            <h3 className="text-cyanBlack font-Nunito font-black text-5xl md:text-start text-center md:max-w-[534px] leading-[120%]">
+          <div className="lg:w-6/12 w-full lg:mt-0 mt-32 lg:mx-0 mx-auto">
+            <h3 className="text-cyanBlack font-Nunito font-black xl:text-5xl sm:text-[45px] text-3xl lg:text-start text-center lg:max-w-[534px] leading-[120%]">
               Stabilise & continue
               <span className="linear_color2"> the pump</span>
             </h3>
-            <p className="text-white opacity-80 font-Montserrat font-normal text-base pt-4 md:max-w-[466px] md:text-start text-center">
+            <p className="text-white opacity-80 font-Montserrat font-normal md:text-base text-sm pt-4 lg:max-w-[466px] lg:text-start text-center">
               When the sell pressure becomes significant the{" "}
               <span className="linear_color">equalizer</span> is a bot that has
               access to the whale wallet and uses it to buy Infinicrease which
