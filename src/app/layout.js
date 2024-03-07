8cdec453b0d9fe659759ac3de55976f51c88cc90
radialcoderavipoonia <ravipoonia28122002@gmail.com>
2024-03-07 16:22:45 +0530

    meta image changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,14 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  metadataBase: "https://infinicrease.vercel.app/",
   title: "Iinfinicrease",
-  description: "Infinicrease",
-  imageUrl: "/assets/images/meta/meta.png",
+  description: "Iinfinicrease",
+  openGraph: {
+    title: "Iinfinicrease",
+    description: "Iinfinicrease",
+    images: ["/assets/images/meta/meta.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
